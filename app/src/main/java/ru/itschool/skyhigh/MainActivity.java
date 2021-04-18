fdc553c3fb5e7ff82271a3aa3d889d2ad72d39be
Popov Daniil <popovdanin@gmail.com>
2021-04-18 17:05:52 +0400

    connecting new libraries for developing (mikepenz and others)

--- app/src/main/java/ru/itschool/skyhigh/MainActivity.java
+++ app/src/main/java/ru/itschool/skyhigh/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mikepenz.materialdrawer.DrawerBuilder;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +13,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 }
